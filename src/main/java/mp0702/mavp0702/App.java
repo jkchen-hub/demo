bc2685250244b2d372925c2636f4b3ae9254edb0
jkchen-hub <1054109772@qq.com>
2019-07-08 10:08:19 +0800

    上传本地Maven项目到GitHub的demo

---
+++ src/main/java/mp0702/mavp0702/App.java
@@ -0,0 +1,17 @@
+package mp0702.mavp0702;
+
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a = ArithmeticUtils.gcd(361,285);
+		System.out.println(a);
+    	System.out.println( "Hello World!" );
+    }
+}
